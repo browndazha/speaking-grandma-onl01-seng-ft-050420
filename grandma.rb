599803ebd1c6357b090dfcab9b16e74c3f37ca33
Dazha Brown <dajahbrown@hotmail.com>
2020-05-09 21:24:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return "I LOVE YOU TOO PUMPKIN!"
+  if phrase == 'WHAT DID YOU EAT TODAY?'
+    return "NO, NOT SINCE 1938!"
+  if phrase == 'WHAT?'
+    return "NO, NOT SINCE 1938!"
   elsif phrase == phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
