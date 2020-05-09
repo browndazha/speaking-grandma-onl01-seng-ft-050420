051d2ce2c6264ef019c55c569b4dee96103a5434
Dazha Brown <dajahbrown@hotmail.com>
2020-05-09 21:11:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,3 @@
-def speak_to_grandma
-  puts 'phrase'# Write a speak_to_grandma method.
-end
-
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA! '
     return "I LOVE YOU TOO PUMPKIN!"
